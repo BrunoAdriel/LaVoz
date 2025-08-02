367bd5007cec9d5f0011e263e31767170c51e403
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2025-08-02 20:47:23 -0300

    Agrego el frot separado del back

---
+++ fontend/js/pack.js
@@ -0,0 +1,322 @@
+const dataRecived = {
+  pack : [],
+    carrier : [
+      {name : "MOVISTAR", id : 1},
+      {name : "PERSONAL", id : 2},
+      {name : "CLARO", id : 3},
+    ],
+    plataforma : [
+      {name : "Mercado Pago", id : 1, url: "/packs"},
+      {name : "Pago360", id : 2, url: "/packs"},
+      {name : "Mensaje de Texto", id : 3, url: "/packs" } 
+    ],
+      id_producto: 501,
+      id_trivia: 501
+}
+
+
+//Conductores
+const cardsContainer =document.getElementById("cardsContainer");
+const medioPago = document.getElementById("medioPago");
+const seccionCard = document.getElementById('seccionCard');
+const seccionPago = document.getElementById('seccionPago');
+const seccionFormulario = document.getElementById('seccionFormulario');
+const cerrarModalPago = document.getElementById('cerrarModalPago');
+const cerrarModalForm = document.getElementById('cerrarModalForm');
+
+//Variables
+let packSeleccionado = null;
+let plataformaSeleccionada = null;
+let idMedioPago = null;
+let opcionElegida = "";
+
+async function ObtenerPacks(){
+  try{
+    // Realizar la solicitud al backend
+    const res = await  fetch("http://localhost:3000/packs",)
+    const data = await res.json();
+    const packs = data.pack || [];
+    dataRecived.pack = packs;
+    return packs; 
+  }catch(error){
+    console.error("Error al obtener los packs de pregunta", error);
+    return;
+  }
+}
+
+
+async function CrearCards(){
+    try {
+    const packs = await ObtenerPacks(); 
+    cardsContainer.innerHTML = "";
+
+    packs.forEach(p=>{
+    //Inyecto las cards
+    const cardHTMT =`
+    <div class="col">
+      <div class="card card-custom" id="card-${p.ID_ITEM}">
+        <section class="header-custom">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA24Z-JDbwSxxVtbqy07-Sq7nulnXMN5YcgA&s" alt="${p.ITEM}" class="card-img-top"/>
+          <h5 class="card-title">${p.ITEM}</h5>
+        </section>
+        <section class="card-body d-none">
+          <p class="card-text">${p.DESCRIPCION}</p>
+          <p class="card-text"><small>Valor: $${p.ITEM_PRICE}</small></p>
+          <p hidden>${p.ITEM_CANT}</p>
+            <section class="card-footer d-flex justify-content-center">
+              <button class="btn btn-compra btn-principal btn-outline-primary mt-3" id-data-pack="${p.ID_ITEM}" >Comprar preguntas</button>
+            </section>
+        </section>
+      </div>
+    </div>
+    `;
+    cardsContainer.innerHTML += cardHTMT;
+    });
+
+  /* Codigo para que se vea todo el contenido de la CARD */
+  document.querySelectorAll(".card-body").forEach(body => {
+    body.classList.remove("d-none");
+  });
+
+    //Agregar eventos en los btn
+    setTimeout(() => {
+      document.querySelectorAll(".btn-compra").forEach(btn => {
+        btn.addEventListener("click", (e) => {
+          seccionCard.classList.add("d-none");
+          seccionPago.classList.remove("d-none");
+
+          // Inyectar botones de pago
+          const packID = parseInt(e.target.getAttribute('id-data-pack'), 10);
+          const item = dataRecived.pack.find(p => p.ID_ITEM=== packID);
+          //Capturo los elementos para enviar
+            if (item) {
+              packSeleccionado = {
+                id_ITEM: item.ID_ITEM,
+                ITEM_PRICE: item.ITEM_PRICE,
+                ITEM_CANT: item.ITEM_CANT 
+              };
+            }
+          crearBtn(dataRecived.plataforma);
+        });
+      });
+    }, 0);
+
+} catch (error) {
+    console.error("Error al cargar packs:", error);
+    cardsContainer.innerHTML = `<div class="alert alert-danger">No se pudieron cargar los packs.</div>`;
+  }
+}
+
+CrearCards();
+
+//inyecto los btn de pago
+function crearBtn(plataforma) {
+  medioPago.innerHTML = plataforma.map(plat => `
+    <button class="btn btn-outline-primary option-btn btn-principal m-2" data-option="${plat.name}" data-id="${plat.id}">${plat.name}</button>
+  `).join('');
+
+    document.querySelectorAll('.option-btn').forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        const opcion = e.target.getAttribute('data-option');
+        const id = parseInt(e.target.getAttribute('data-id'), 10);
+
+        if (opcion === "Mensaje de Texto") {
+          window.location.href = "";
+          console.log("Redireccion:", window.location.href)
+          return;
+        }
+
+// Guardar selección
+    window.opcionElegida = opcion;
+    window.idMedioPago = id;
+
+    // Modificar el contenido del H4 resumen
+    const resumenVoto = document.getElementById("resumenVoto");
+    if (resumenVoto && packSeleccionado && opcion) {
+      resumenVoto.innerText = `Comprar pack de ${packSeleccionado.ITEM_CANT} preguntas con ${opcion}`;
+    }
+
+    // Ocultar sección de pago, mostrar formulario
+    document.getElementById('seccionPago').classList.add('d-none');
+    document.getElementById('seccionFormulario').classList.remove('d-none');
+      });
+    });
+}
+
+//Inyecto las operadoras
+
+function cargarOperadoras(carriers) {
+  const selectOperadora = document.getElementById('operadora');
+
+  carriers.forEach(carrier => {
+    const option = document.createElement('option');
+    option.value = carrier.id;
+    option.textContent = carrier.name.charAt(0).toUpperCase() + carrier.name.slice(1); 
+    selectOperadora.appendChild(option);
+  });
+};
+
+cargarOperadoras(dataRecived.carrier);
+
+
+//funcion cerrar modal
+function manejarCerrarModal(e) {
+  const id = e.target.id;
+
+  if (id === "cerrarModalForm") {
+    seccionFormulario.classList.add("d-none");
+    seccionPago.classList.remove("d-none");
+  } else if (id === "cerrarModalPago") {
+    seccionPago.classList.add("d-none");
+    seccionCard.classList.remove("d-none");
+  }
+}
+// Listeners
+cerrarModalForm.addEventListener("click", manejarCerrarModal);
+cerrarModalPago.addEventListener("click", manejarCerrarModal);
+
+
+//Restricciones de letras y caracteres especiales del imput telefono 
+const telefonoInput = document.getElementById('telefono');
+
+telefonoInput.addEventListener('input', (e) => {
+  e.target.value = e.target.value.replace(/[^\d]/g, '');
+});
+
+// Enviar formulario
+const voteForm = document.getElementById('voteForm');
+voteForm.addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  const telefono = document.getElementById('telefono').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const operadora = parseInt(document.getElementById('operadora').value, 10);
+
+  const telefonoRegex = /^[0-9]{10,15}$/;
+  const emailRegex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+
+  if (!telefonoRegex.test(telefono)) {
+    document.getElementById('formErrors').innerText = "Número inválido.";
+    return;
+  }
+
+  if (!emailRegex.test(email)) {
+    document.getElementById('formErrors').innerText = "Email inválido.";
+    return;
+  }
+
+  if (!operadora || operadora === "Seleccioná") {
+    document.getElementById('formErrors').innerText = "Elegí una operadora.";
+    return;
+  }
+
+  //al clickear el btn, se va y aparece el spinner
+  document.getElementById('btnEnviar').classList.add('d-none');
+  document.getElementById('postSubmit').classList.remove('d-none');
+ 
+
+  // Datos a enviar
+  const datos = { 
+    id_plataforma: window.idMedioPago, 
+    id_trivia: dataRecived.id_trivia,
+    id_item: packSeleccionado.id_ITEM,
+    item_cant: packSeleccionado.ITEM_CANT,
+    item_price: packSeleccionado.ITEM_PRICE,
+    ani: telefono,
+    email: email,
+    id_carrier: operadora,
+    id_producto: dataRecived.id_producto
+  };
+
+  await enviarPago(datos, window.opcionElegida);
+});
+
+//Mostrar el Toast segun el estado
+function mostrarToast(mensaje, tipo) {
+  let color = {
+    success: "linear-gradient(to right, #00b09b, #96c93d)",
+    error: "linear-gradient(to right, #d32f2f, #ff5252)",
+    warning: "linear-gradient(to right, #f7b733, #fc4a1a)"
+  }; 
+
+  Toastify({
+    text: mensaje,
+    style : { background: color[tipo] }|| "linear-gradient(to right, #00c6ff, #0072ff)",
+    duration: 3000,
+    gravity: "center",
+    position: "center",
+    close: true
+  }).showToast();
+};
+
+//Extrae el valor de los parametros de la URL
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const flag = urlParams.get('flag');
+
+//Selecciona segun la operacion
+if (flag === 'success') {
+  mostrarToast("Operación exitosa", "success");
+} else if (flag === 'failure' || flag === 'fail') {
+  mostrarToast("Ocurrió un error", "error");
+} else if (flag === 'pendding') {
+  mostrarToast("Tu operación está pendiente", "warning");
+}
+
+//Funcion para capturar la informacion de la plataforma
+function plataformaData(nombre){
+  return dataRecived.plataforma.find(p => p.name === nombre)
+};
+
+// Función para enviar los datos al backend
+async function enviarPago(datos, metodoPago) {  
+    const plataformaSeleccionada = plataformaData(metodoPago); 
+
+    //Si el metodoPago elegido tiene distinta URL al seleccionado
+    if(!plataformaSeleccionada || !plataformaSeleccionada.url){
+    document.getElementById("formErrors").innerText = "Método de pago no válido.";
+    return;
+  }
+
+  try {
+    const response = await fetch('http://localhost:3000/packs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(datos)
+  });
+    const contentType = response.headers.get("Content-Type") || "";
+    if (!response.ok) {
+      console.log("rta", response)
+      console.error("Error HTTP:", response.status);
+      const errorText = await response.text(); 
+      console.error("Error HTTP:", response.status, errorText);
+      document.getElementById("formErrors").innerText = `Error del servidor (${response.status})`;
+      mostrarToast("Ocurrió un Error el procesar tu pago", "error");
+      return;
+    }
+
+    if (contentType.includes("application/json")) {
+      const data = await response.json(); 
+      console.log("Respuesta JSON:", data);
+
+      if (data.status && data.result) {
+        const redireccion = data.result.init_point || data.result.checkout_url; 
+        console.log("Redireccion",redireccion)
+
+        mostrarToast("Formulario enviado correctamente", "success");
+
+        //window.location.href = redireccion; 
+
+      }
+    } else {
+      const errorText = await response.text();
+      document.getElementById("formErrors").innerText = "Respuesta inesperada del servidor.";
+      mostrarToast("Respuesta inesperada del servidor", "warning");
+    }
+  } catch (error) {
+    console.error("Error en fetch:", error);
+    document.getElementById("formErrors").innerText = "Error de conexión con el servidor.";
+    document.getElementById("postSubmit").classList.add("hidden"); 
+    mostrarToast("Error en la coneccion del servidor", "error");
+  }
+}
---
+++ fontend/js/trivia.js
@@ -0,0 +1,340 @@
+
+// Almacenadores
+let respuestaSeleccionada = null;
+let numeroIngresado = null;
+
+// Conductores
+const btnIniciarTrivia = document.getElementById('iniciarTrivia');
+const triviaContainer = document.getElementById('triviaContainer');
+const preguntaActual = document.getElementById('preguntaActual');
+const opcionesContainer = document.getElementById('opciones');
+const btnEnviar = document.getElementById('btnEnviar');
+const contadorPreguntas = document.getElementById('contadorPreguntas');
+const puntosObtenidos = document.getElementById('puntosObtenidos');
+const resultadoFinal = document.getElementById('resultadoFinal');
+const btnCerrarModal = document.getElementById('cerrarModal');
+const inputTelefono =   document.getElementById('inputTelefono');
+const crosselingContainer = document.getElementById('crosselingContainer');
+const dataParticipante = document.getElementById('dataParticipante');
+const spinnerSubmit =  document.querySelector('#spinnerSubmit');
+const tituloTrivia = document.getElementById('tituloTrivia');
+
+//Logica para inyectar el estilo en la clase segun el resultado
+function mostrarToast(mensaje, tipo) {
+let color = {
+    success: "linear-gradient(to right, #00b09b, #96c93d)",
+    error: "linear-gradient(to right, #ff5f6d, #ffc371)",
+    warning: "linear-gradient(to right, #f7b733, #fc4a1a)"
+  }; 
+
+  Toastify({
+    text: mensaje,
+    duration: 3000,
+    gravity: "center",
+    position: "center",   
+    style: {
+      background: color[tipo] || "linear-gradient(to right, #00c6ff, #0072ff)",
+    }, 
+    stopOnFocus: true
+  }).showToast();
+}
+
+//Funcion para capturar el usuario 
+/* async function validarUsuario(ani) {
+  try{
+    const res = await fetch(`${API_ANI}/501/active/${ani}`, {
+      method : "POST",
+      headers : {'Content-Type' : 'application/json',
+                  'Authorization' : `Bearer ${tokenTrivia}`
+      },
+    })
+    const data = await res.json();
+    console.log("informacion de api:", data);
+
+    //Si no me llegan 3 parametros principales
+    if (!data.status || !data.result || data.statusCode !== 201) {
+      return null;
+    } else {
+    // Devuelvo del objeto los elementos que necesito
+    return {
+      ani: data.result.ANI_CLIENTE,
+      triviaActiva: data.active,
+      name: `${data.result.ANI_CLIENTE}` 
+    };
+    }
+
+  }catch(error){
+    console.error("Error interno en el servidor.", error);
+    throw new Error('Fallo interno en el servidor');
+  }
+}  */
+
+// Funcion para mostrar mensajes
+function mensajeUsuario(usuario, numeroIngresado){
+
+  const ingreso = `
+    <section>
+      <p>Ani N.º ${usuario.name}!</p>
+    </section>
+  `;
+
+  //Validacion para guardar el ani en el LocalStorage
+  if(usuario.triviaActiva||!usuario.triviaActiva){
+    localStorage.setItem("NumeroGuardado", numeroIngresado);
+  }
+
+  if(usuario.triviaActiva){
+  mostrarToast("✅ Acceso concedido: tenés trivia activa.", "success");
+  document.getElementById('iniciarTrivia').classList.remove('d-none');
+  document.getElementById('iniciarTrivia').disabled = false;
+  inputTelefono.classList.add('d-none');
+  dataParticipante.classList.remove('d-none');
+    dataParticipante.innerHTML =`
+      <!--Btn para cerrar el modal-->
+      <span class="close-btn" id="btnCerrarInterno">&times;</span>
+      ${ingreso}
+      `;
+  }else if(!usuario.triviaActiva){
+    mostrarToast('✅Acceso concedido: No tenes trivias activas!', 'success');
+    inputTelefono.classList.add('d-none');
+    dataParticipante.classList.remove('d-none');
+    dataParticipante.innerHTML = `
+      <!--Btn para cerrar el modal-->
+      <span class="close-btn" id="btnCerrarInterno">&times;</span>
+        <div class="text-center position-relative">
+          ${ingreso}
+          <section class="alert alert-danger text-center" style="max-width: auto;" >
+            <p class="mb-2">No se encontraron trivias activas vinculadas a tu usuario, por favor compra un pack para continuar respondiendo</p>
+            <a class='link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover' href="/packs" target='_blank' rel='noopener noreferrer'>Comprá más preguntas👈</a>
+          </section>
+        </div>
+        `;  
+  }
+
+  //Escucha del btn Cerrar
+  const btnCerrarInterno = document.getElementById('btnCerrarInterno');
+  btnCerrarInterno.addEventListener('click', cerrarTrivia);
+}
+
+//Consumo el listener, guardo el resultado en una variable, lo envio para hacer las validaciones
+document.getElementById('validarTelefono').addEventListener('click', async() => {
+  numeroIngresado = document.getElementById('telefono').value.trim();
+
+  if(!numeroIngresado){
+    mostrarToast('⚠ Ingresá un número de teléfono.', 'warning');
+    return;
+  }
+
+  try{
+    const usuario = await validarUsuario(numeroIngresado);
+
+    if(!usuario){
+    mostrarToast('❌ Usuario no registrado.', 'warning');
+    inputTelefono.classList.remove('d-none');
+    return;
+    }
+
+    mensajeUsuario(usuario, numeroIngresado);
+  }catch(error){
+    console.error("Error interno en el servior.",error);
+    throw new Error("Fallo interno en el servidor.");
+  }
+});
+
+// Iniciar Trivia
+btnIniciarTrivia.addEventListener('click', async () => {
+  // Ocultar input y botón de validación del telefeno
+  inputTelefono.classList.add('d-none');
+  btnIniciarTrivia.classList.add('d-none');
+  dataParticipante.classList.add('d-none');
+  triviaContainer.classList.remove('d-none');
+  await cargarPregunta();
+});
+
+
+//Funcion cargar pregunta
+async function cargarPregunta(){
+  try{
+    //Consumo de la API 
+/*     let url = `${API_URL_TRIVIA}/pregunta/${numeroIngresado}`;
+    const res = await fetch(url, {
+      method : "POST",
+      headers : {'Content-Type' : 'application/json',
+                  'Authorization' : `Bearer ${tokenTrivia}`
+      },
+      body: JSON.stringify({
+            id_trivia: 501,
+            ani: numeroIngresado
+      })
+    });
+    pregunta = await res.json();
+    console.log("Contenido preg:",pregunta); */
+
+    //Guardo el contenido de pregunta, si este viene vacio o sin ID finaliza
+    const preguntaData = pregunta.result.PREGUNTA;
+    if (!preguntaData || !preguntaData.ID_PREGUNTA) {
+      return finalizarTrivia({
+        puntosFinales: pregunta.result.PUNTOS || 0,
+        totalRespondido: pregunta.result.TOTAL || 0,
+      });
+    }
+
+    //Objeto fictisio crossling
+    pregunta.result.CROSSELING={
+      mensaje:"❌Te estas quedando sin preguntas!",
+      link:"/packs"
+    }  
+
+    triviaContainer.classList.remove('d-none');
+
+    mostrarPregunta(pregunta);
+  }catch(error){
+    console.error(error, "Se produjo un error al cargar las preguntas");
+    finalizarTrivia();
+  }
+};
+
+// Mostrar pregunta
+function mostrarPregunta(pregunta) {
+  preguntaActual.textContent = pregunta.result.PREGUNTA.PREGUNTA;
+  puntosObtenidos.textContent = `Puntos: ${pregunta.result.PUNTOS}`;
+  contadorPreguntas.textContent = `Pregunta: ${pregunta.result.RESTANTE}/${pregunta.result.TOTAL}`;
+  crosselingPacks(pregunta.result.RESTANTE, pregunta.result.TOTAL, pregunta.result.CROSSELING)
+  respuestaSeleccionada = null;
+  btnEnviar.classList.add('d-none');
+  opcionesContainer.innerHTML = '';
+
+  //Combierte en array y les asigna una clave/valor, 
+  Object.entries(pregunta.result.PREGUNTA.RESPUESTAS).forEach(([letra, opcion])=> { 
+    const btn = document.createElement('button');
+    btn.className = 'btn btn-outline-danger opcion-btn';
+    btn.textContent = ` ${opcion}`;
+
+    btn.addEventListener('click', () => {
+      respuestaSeleccionada = letra;
+      document.querySelectorAll('#opciones button').forEach(b => b.classList.remove('active'));
+      btn.classList.add('active');
+      btnEnviar.classList.remove('d-none');
+    });
+
+    opcionesContainer.appendChild(btn);
+  });
+}
+
+// Enviar respuesta
+btnEnviar.addEventListener('click', async () => {
+  try{  
+    //Desactivo el btn mientras esta haciendo el envio
+    btnEnviar.disabled= true;
+    spinnerSubmit.classList.remove('d-none');
+
+    //Envia respuesta a la API
+/*     const url = `${API_URL_TRIVIA}/respuesta/${numeroIngresado}`;
+    const res = await fetch(url, {
+    method : 'POST',
+    headers: {  'Content-Type': 'application/json',
+                'Authorization': `Bearer ${tokenTrivia}`
+     },
+    body: JSON.stringify({
+      id_trivia: pregunta.result.PREGUNTA.ID_TRIVIA,
+      id_pregunta: pregunta.result.PREGUNTA.ID_PREGUNTA,
+      respuesta: respuestaSeleccionada,
+      ani: numeroIngresado
+    })
+  });   */
+  
+  const data = await res.json();
+  console.log('data traida:',data)
+
+  await cargarPregunta(data?.result?.PREGUNTA?.ID_PREGUNTA);
+
+  }catch(error){
+    console.error(error,"Error al enviar respuesta al servidor");
+  } finally{
+    spinnerSubmit.classList.add('d-none');
+    btnEnviar.disabled = false;
+  }
+});
+
+// Funcion cerrar modal
+function cerrarTrivia(){
+  // Mostrar input 
+  inputTelefono.classList.remove('d-none');
+
+  triviaContainer.classList.add('d-none');     
+  btnIniciarTrivia.classList.add('d-none');
+  dataParticipante.classList.add('d-none');  
+  crosselingContainer.classList.add('d-none');
+  resultadoFinal.classList.add('d-none');
+
+  // Deshabilitar el btn trivia
+  btnIniciarTrivia.disabled = false;
+}
+
+btnCerrarModal.addEventListener('click', cerrarTrivia);
+
+// Finalizar
+function finalizarTrivia({puntosFinales=0, totalRespondido=0}= {}) {
+
+  triviaContainer.classList.add('d-none');
+  tituloTrivia.classList.add('d-none');
+  resultadoFinal.classList.remove('d-none');
+
+  resultadoFinal.innerHTML = `
+    <h3>Trivia finalizada 🎉</h3>
+    <p class='mt-4'>Respondiste <strong> ${totalRespondido} preguntas </strong></p>
+    <p>Total de puntos obtenidos en la trivia: <strong>${puntosFinales}</strong></p>
+    <button class="btn btn-primary mt-3 btn-principal btn-lg" onclick="reiniciarTrivia()">Continuar Respondiendo</button>
+  `;
+}
+
+// Reiniciar
+function reiniciarTrivia() {
+  btnIniciarTrivia.classList.add('d-none');
+  resultadoFinal.classList.add('d-none');
+  triviaContainer.classList.add('d-none');
+  inputTelefono.classList.remove('d-none');
+}
+
+
+// Reanudar trivia si hay progreso guardado
+window.addEventListener('DOMContentLoaded', () => {
+  inputTelefono.classList.remove('d-none');
+
+  // Deshabilitar el botón de iniciar trivia hasta validar
+  btnIniciarTrivia.disabled = true;
+});
+
+//Crosseling para enviar a la seccion de PACKS
+function crosselingPacks(dataCrosseling){
+  if(dataCrosseling && dataCrosseling.link && dataCrosseling.mensaje){
+    crosselingContainer.classList.remove('d-none');
+    crosselingContainer.querySelector('p').innerHTML = `
+      ${dataCrosseling.mensaje} 
+      <a class='link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover' href="${dataCrosseling.link}" target='_blank' rel='noopener noreferrer'>Comprá más ahora 👈</a>
+    `;
+  }else {
+    crosselingContainer.classList.add('d-none');
+  }
+}
+
+// Usar el Numero guardado al recargar la pagina
+document.addEventListener("DOMContentLoaded", () => {
+  const numeroGuardado = localStorage.getItem("NumeroGuardado");
+  if (numeroGuardado) {
+    document.getElementById('telefono').value = numeroGuardado;
+  }
+});
+
+//Flags de la URL
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const flag = urlParams.get('flag');
+//Selecciona segun la operacion
+if (flag === 'success') {
+  mostrarToast("Operación exitosa", "success");
+} else if (flag === 'failure' || flag === 'fail') {
+  mostrarToast("Ocurrió un error", "error");
+} else if (flag === 'pendding') {
+  mostrarToast("Tu operación está pendiente", "warning");
+}
