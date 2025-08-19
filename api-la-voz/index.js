90a604768227aaf59c48a809114bf94c18d37b77
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2025-08-19 13:44:13 -0300

    modifico configuracion de render

--- api-la-voz/index.js
+++ api-la-voz/index.js
@@ -213,6 +213,9 @@ app.post('/preguntas/responder', async (req, res) => {
     }
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'fontend', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
